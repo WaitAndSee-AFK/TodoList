bc1c891376e80a687cecc2b766eab4f43b2495c5
LELIK <b_banni@inbox.ru>
2025-09-16 18:44:02 +0300

    A singleton has been created

---
+++ app/src/main/java/com/example/todolist/Database.kt
@@ -0,0 +1,40 @@
+    package com.example.todolist
+
+    import kotlin.random.Random
+
+
+    object Database {
+
+//        private lateinit var instance: Database
+////
+////        fun getInstance(): Database {
+////            if (!::instance.isInitialized) {
+////                instance = Database
+////            }
+////            return instance
+////        }
+
+        private val _notes = arrayListOf<Note>()
+        val notes
+            get() = _notes.toList()
+
+        init {
+            for (i in 0..20) {
+                val note = Note(i, "Note $i", Random.nextInt(3))
+                _notes.add(note)
+            }
+        }
+
+        fun add(note: Note) {
+            _notes.add(note)
+        }
+
+        fun remove(id: Int) {
+            for (i in 0.._notes.size) {
+                val note = _notes[i]
+                if (note.id ==  id) {
+                    remove(i)
+                }
+            }
+        }
+    }
